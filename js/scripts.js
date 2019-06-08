040cf9ea14188d5a768a021b3b666ca075cabf60
minar-tech <mika.nyman@gmx.com>
2019-06-08 03:00:43 +0300

    Edit

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,8 @@ function myFunction() {
   }
 }
 
-
 // ---------- GET query variable ----------
 /* https://css-tricks.com/snippets/javascript/get-url-variables/ */
-/*
 function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
   var vars = query.split("&");
@@ -24,38 +22,37 @@ function getQueryVariable(variable) {
   return (false);
 }
 var p = getQueryVariable("p"); // --> funding
-var full_url = cur_url + "?p=" + p;
-var p_filename = p + ".html"; // --> funding.html
-*/
+//var p_filename = p + ".html"; // --> funding.html
 
+function activateNaviButton(param) {
+  // ---------- GET query variable for p ----------
+  var query = window.location.search.substring(param); // --> ?p=funding
+  var file_w_param = "t1.html" + query // --> t1.html?p=home
 
-// ---------- GET query variable for p ----------
-// --> ?p=funding
-var query = window.location.search.substring('p');
-// --> page.html?p=funding
-var file_w_param = "page.html" + query
+  // ---------- Make navi button active ----------
+  var url = window.location.pathname;
+  //var filename = url.substring(url.lastIndexOf('/') + 1); // --> page.html
+  // Get the container element
+  var btnContainer = document.getElementById("myTopnav");
+  // Get all buttons with class="btn" inside the container
+  var btns = btnContainer.getElementsByClassName("btn");
 
-
-// ---------- Make navi button active ----------
-var url = window.location.pathname;
-var filename = url.substring(url.lastIndexOf('/') + 1); // --> page.html
-// Get the container element
-var btnContainer = document.getElementById("myTopnav");
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-// Loop through the buttons and add the active class to the current/clicked button
-var cur_url, cur_path, cur_file;
-for (var i = 0; i < btns.length; i++) {
-  // --> http://localhost/pages/page.html?p=funding
-  cur_url = btns[i];
-  // --> /pages/page.html?p=funding
-  cur_path = cur_url.getAttribute("href");
-  // --> page.html?p=funding
-  cur_file = cur_path.substring(cur_path.lastIndexOf('/') + 1);
-  if (cur_file == file_w_param) {
-    cur_url.className += " active";
+  // Loop through the buttons and add the active class to the current/clicked button
+  var cur_url, cur_path, cur_file;
+  for (var i = 0; i < btns.length; i++) {
+    cur_url = btns[i]; // --> http://localhost/pages/page.html?p=funding
+    cur_path = cur_url.getAttribute("href"); // --> /pages/page.html?p=funding
+    cur_file = cur_path.substring(cur_path.lastIndexOf('/') + 1); // --> page.html?p=funding
+    if (cur_file == file_w_param) {
+      cur_url.className += " active";
+    }
   }
 }
+activateNaviButton(p);
+
 // Use filename for include
-include_content = "<div w3-include-html=\"/texts/text_" + filename + "\"></div>"
-document.getElementById("include").innerHTML = include_content;
+function callInclude(param) {
+  include_content = "<div w3-include-html=\"/content/" + param + ".html\"></div>"
+  document.getElementById("include").innerHTML = include_content;
+}
+callInclude(p);
